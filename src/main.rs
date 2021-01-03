974564bb4ee38c5a869a0dd29bbbaf73b4d771da
G1gg1L3s <mr.a678i@gmail.com>
2021-01-03 16:15:49 +0200

    Add main for testing parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+#![feature(iter_advance_by)]
+use crate::parser::{lexer::Lexer, parser::Parser};
+
+mod parser;
+
 fn main() {
-    println!("Hello, world!");
+    let file = std::env::args().nth(1).expect("expect filename");
+    let input = std::fs::read_to_string(file).unwrap();
+    let (tokens, _) = Lexer::new(&input).tokenize();
+    let ast = Parser::new(&input, tokens).parse();
+    println!("{:#?}", ast);
 }
