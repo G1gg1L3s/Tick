febb206be1ea0dc306c3f75b6d854324f48d2246
G1gg1L3s <mr.a678i@gmail.com>
2021-01-04 17:40:19 +0200

    Move infix parsing to dedicated method

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -11,6 +11,31 @@ pub struct Parser<'a> {
     token: Token,
 }
 
+type PrecPair = (i32, i32);
+
+const OR: PrecPair = (1, 2);
+const AND: PrecPair = (3, 4);
+const COMP: PrecPair = (5, 6);
+const ADD: PrecPair = (7, 8);
+const MUL: PrecPair = (9, 10);
+const AS: PrecPair = (12, 11);
+const UN: i32 = 13;
+const FIELD: PrecPair = (15, 16);
+
+impl TokenKind {
+    fn infix_binding_power(&self) -> PrecPair {
+        match self {
+            TokenKind::OrOr => OR,
+            TokenKind::AndAnd => AND,
+            TokenKind::LE | TokenKind::Lt | TokenKind::EqEq | TokenKind::GE | TokenKind::Gt => COMP,
+            TokenKind::Plus | TokenKind::Minus => ADD,
+            TokenKind::Star | TokenKind::Slash => MUL,
+            TokenKind::Dot => FIELD,
+            _ => (0, 0),
+        }
+    }
+}
+
 impl<'a> Parser<'a> {
     pub fn new(src: &'a str, tokens: Vec<Token>) -> Self {
         Self {
@@ -30,10 +55,11 @@ impl<'a> Parser<'a> {
         self.idx += 1;
     }
 
-    fn consume(&mut self, kind: TokenKind, expect: &str) -> PResult<()> {
+    fn consume(&mut self, kind: TokenKind, expect: &str) -> PResult<Token> {
         if self.token.is(kind) {
+            let tk = self.token;
             self.bump();
-            Ok(())
+            Ok(tk)
         } else {
             Err(PError::new(format!("Expect {}", expect), self.token.span))
         }
@@ -54,7 +80,7 @@ impl<'a> Parser<'a> {
     }
 
     fn parse_expr(&mut self) -> PResult<Expr> {
-        self.parse_expr_with(0)
+        self.parse_expr_with(1)
     }
 
     fn parse_lit(&mut self) -> PResult<Expr> {
@@ -66,7 +92,7 @@ impl<'a> Parser<'a> {
     fn parse_un(&mut self, op: UnOp) -> PResult<Expr> {
         let lo = self.token.span;
         self.bump();
-        let expr = self.parse_prefix_expr()?;
+        let expr = self.parse_expr_with(UN)?;
         let span = lo.to(expr.span);
         Ok(Expr::new_un(span, op, expr))
     }
@@ -84,26 +110,47 @@ impl<'a> Parser<'a> {
 
     fn parse_expr_with(&mut self, min_prec: i32) -> PResult<Expr> {
         let mut lhs = self.parse_prefix_expr()?;
-        loop {
-            let op = match self.token.kind {
-                TokenKind::Plus => BinOp::Add,
-                TokenKind::Minus => BinOp::Sub,
-                TokenKind::Star => BinOp::Mul,
-                TokenKind::Slash => BinOp::Div,
-                _ => break,
 
-            };
-            let (left_prec, right_prec) = op.infix_binding_power();
+        loop {
+            let (left_prec, right_prec) = self.token.kind.infix_binding_power();
             if left_prec < min_prec {
                 break;
             }
-            self.bump();
-            let rhs = self.parse_expr_with(right_prec)?;
-            let span = lhs.span.to(rhs.span);
-            lhs = Expr::new_bin(span, op, lhs, rhs);
+            lhs = self.parse_infix(lhs, right_prec)?;
         }
         Ok(lhs)
     }
+
+    fn parse_infix(&mut self, lhs: Expr, right_prec: i32) -> PResult<Expr> {
+        match self.token.kind {
+            TokenKind::Plus => self.parse_bin(BinOp::Add, lhs, right_prec),
+            TokenKind::Minus => self.parse_bin(BinOp::Sub, lhs, right_prec),
+            TokenKind::Star => self.parse_bin(BinOp::Mul, lhs, right_prec),
+            TokenKind::Slash => self.parse_bin(BinOp::Div, lhs, right_prec),
+            TokenKind::AndAnd => self.parse_bin(BinOp::And, lhs, right_prec),
+            TokenKind::OrOr => self.parse_bin(BinOp::Or, lhs, right_prec),
+            TokenKind::Lt => self.parse_bin(BinOp::Lt, lhs, right_prec),
+            TokenKind::LE => self.parse_bin(BinOp::Le, lhs, right_prec),
+            TokenKind::EqEq => self.parse_bin(BinOp::Eq, lhs, right_prec),
+            TokenKind::Gt => self.parse_bin(BinOp::Gt, lhs, right_prec),
+            TokenKind::GE => self.parse_bin(BinOp::Ge, lhs, right_prec),
+
+            TokenKind::Dot => self.parse_field(lhs, right_prec),
+            _ => unimplemented!(),
+        }
+    }
+
+    fn parse_bin(&mut self, op: BinOp, lhs: Expr, right_prec: i32) -> PResult<Expr> {
+        self.bump();
+        let rhs = self.parse_expr_with(right_prec)?;
+        let span = lhs.span.to(rhs.span);
+        Ok(Expr::new_bin(span, op, lhs, rhs))
+    }
+
+    fn parse_field(&mut self, lhs: Expr, _: i32) -> PResult<Expr> {
+        unimplemented!()
+    }
+
 
     fn parse_grouping(&mut self) -> PResult<Expr> {
         self.consume(TokenKind::OpenParen, "(")?;
