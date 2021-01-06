e0da69404001c74e4ad7fcb06c60b87e72aea128
G1gg1L3s <mr.a678i@gmail.com>
2021-01-06 23:02:40 +0200

    Add prettier debug message for errors in main

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,29 @@ use crate::parser::{
 mod parser;
 
 fn main() {
-    let file = std::env::args().nth(1).expect("expect filename");
-    let input = std::fs::read_to_string(file).unwrap();
+    let file = "test.tk"; //std::env::args().nth(1).expect("expect filename");
+    let input = std::fs::read_to_string(&file).unwrap();
     let (tokens, _) = Lexer::new(&input).tokenize();
-    let ast = Parser::new(&input, tokens).parse().unwrap();
-    let mut formatter = DebugFormatter::new(&input);
-    formatter.visit_expr(&ast);
+    let items = Parser::new(&input, tokens).parse();
+    match items {
+        Ok(items) => {
+            let mut formatter = DebugFormatter::new(&input);
+            items.iter().for_each(|item| formatter.visit_item(item));
+        }
+        Err(err) => {
+            let line_num = input[..err.span.lo as usize].lines().count() - 1;
+            let line = input.lines().nth(line_num).unwrap();
+            let content = err.span.extract(&input);
+            let content_in_chars = content.chars().count().max(1);
+            let prefix_in_chars = line.chars().count() - content_in_chars;
+            let prefix = " ".repeat(prefix_in_chars + 1);
+            let mark = "~".repeat(content_in_chars);
+            println!(
+                "Error at {}:{}:{}: {}",
+                file, line_num, prefix_in_chars, err.msg
+            );
+            println!("{}", line);
+            println!("{}{}", prefix, mark);
+        }
+    }
 }
